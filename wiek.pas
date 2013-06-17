c3dbfda40d95e37f071146c57729bc571c4cd98e
Marta Czarnocka-Cieciura <martaczc@interia.pl>
2013-06-17 12:48:05 +0200

    debuging

--- wiek.pas
+++ wiek.pas
@@ -12,16 +12,16 @@ const
  nar=100; //liczba sprawdzanych wartosci ar
  as0=0;//?
  ar0=0;//?
- deltaas=0.01; //?
- deltaar=-0.01; //?
+ deltaas=0.002; //?
+ deltaar=-0.002; //?
  czas=200;
 
-Type population=array[1..czas+1] of real;
+Type population=array[1..(czas+1)] of real;
 
 Var pop0, pop1: population;
     t, i, j, k, koh: longint;
     as, ar, bs, br, ps,pr,N, age:real;
-    avgAge,sdAge,avgN,sdN: array[0..4] of real;
+    avgAge,varAge,avgN,varN: array[0..4] of real;
     winter: boolean;
     INFO: text;
 
@@ -29,7 +29,7 @@ begin
 assign(INFO,'wiek.txt');
 rewrite(INFO);
 append(INFO);
-writeln(INFO,'as ','ar ','bs ','br ','t ','avgN[t] ','sdN[t] ','avgAge[t] ','sdAge[t] ');
+writeln(INFO,'as ','ar ','bs ','br ','t ','avgN[t] ','varN[t] ','avgAge[t] ','varAge[t]');
 close(INFO);
 for i:=0 to nas do
  begin
@@ -42,52 +42,51 @@ for i:=0 to nas do
   koh:=1;
   pop0[1]:=20;
   N:=20;
-  writeln(i,' ',j ,' pr(Ne)=',1/(1+exp(-(ar*N+br))):7:3,' ps(Ne)=',1/(1+exp(-(as*N+bs))):7:3);
   for t:=0 to 4 do
    begin
    avgN[t]:=0;
-   sdN[t]:=0;
+   varN[t]:=0;
    avgAge[t]:=0;
-   sdAge[t]:=0;
+   varAge[t]:=0;
    end;
   for t:=1 to czas do
    begin
    if ((t mod 5 = 4) or (t mod 5 = 0)) then winter:=true else winter:=false;
+   //winter:=false;
    age:=0;
    pr:=1/(1+exp(-(ar*N+br)));
    ps:=1/(1+exp(-(as*N+bs)));
    if (not winter) then pop1[1]:=pr*L*N/2 else pop1[1]:=0;
-   N:=pop1[1];
-   for k:=1 to koh do
-    begin
-    pop1[k+1]:=pop0[k]*ps;
-    N:=N+pop1[k+1];
-    age:=age+k*pop1[k+1];
-    end;
+   for k:=1 to koh do pop1[k+1]:=pop0[k]*(1-ps);
    koh:=koh+1;
+   N:=0;
+   for k:=1 to koh do
+    begin 
+    pop0[k]:=pop1[k];
+    age:=age+k*pop1[k];
+    N:=N+pop1[k];
+    end;
    if N<>0 then age:=age/N else age:=0;
    if t>100 then 
     begin
     avgN[t mod 5]:=avgN[t mod 5]+N;
-    sdN[t mod 5]:=sdN[t mod 5]+N*N;
+    varN[t mod 5]:=varN[t mod 5]+N*N;
     avgAge[t mod 5]:=avgAge[t mod 5]+age;
-    sdAge[t mod 5]:=sdAge[t mod 5]+age*age;
+    varAge[t mod 5]:=varAge[t mod 5]+age*age;
     end;
-   for k:=1 to koh do pop0[k]:=pop1[k];
-   //pop0:=pop1;
    gotoxy(1,whereY);
-   write(i,' ',j,' ',t)
+   write(i,' ',j,' ',t);
    end;
+  append(INFO);
   for t:=0 to 4 do
    begin
-   avgN[t]:=avgN[t]/(czas-100);
-   sdN[t]:=sqrt(sdN[t]/(czas-100) - avgN[t]*avgN[t]);
-   avgAge[t]:=avgAge[t]/(czas-100);
-   sdAge[t]:=sqrt(sdAge[t]/(czas-100) - avgAge[t]*avgage[t]);
-   append(INFO);
-   writeln(INFO,as:7:2,' ',ar:7:2,' ',bs:7:5,' ',br:7:5,' ',t,' ',avgN[t]:7:5,' ',sdN[t]:7:5,' ',avgAge[t]:7:5,' ',sdAge[t]:7:5);
-   close(INFO);
+   avgN[t]:=avgN[t]/((czas-100)/5);
+   varN[t]:=varN[t]/((czas-100)/5) - avgN[t]*avgN[t];
+   avgAge[t]:=avgAge[t]/((czas-100)/5);
+   varAge[t]:=varAge[t]/((czas-100)/5) - avgAge[t]*avgage[t];
+   writeln(INFO,as:7:5,' ',ar:7:5,' ',bs:7:5,' ',br:7:5,' ',t,' ',avgN[t]:7:5,' ',varN[t]:7:5,' ',avgAge[t]:7:5,' ',varAge[t]:7:5);
    end;
+  close(INFO);
   end;
  end;
 end.
