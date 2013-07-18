c4ee4796d0dabeae5cad640ece914201155fc82d
Marta Czarnocka-Cieciura <martaczc@interia.pl>
2013-07-18 18:47:25 +0200

    drobne zmiany

--- wiek.pas
+++ wiek.pas
@@ -1,6 +1,6 @@
-{program liczacy spodziewana strukture wiekowa populacji w zaleznosci od wartosci parametrow rozrodu, przy ustalonym zageszczeniu rownowagi i prawdopodobienstwie smierci w stanie rownowagi (model deterministyczny)}
+{program liczacy spodziewany sredni wiek osobnikow i liczebnosc populacji w zaleznosci od wartosci parametrow rozrodu, przy ustalonym zageszczeniu rownowagi i prawdopodobienstwie smierci w stanie rownowagi (model deterministyczny)}
 
-{coś jest nie tak: za mała liczebność populacji, za duże sd wieku i liczebnosci}
+{cos jest nie tak: za mala liczebnosc populacji, za duze sd wieku i liczebnosci}
 
 
 Uses Crt;
@@ -14,14 +14,15 @@ const
  ar0=0;//?
  deltaas=0.002; //?
  deltaar=-0.002; //?
- czas=200;
+ czas=2000;
+ seasons=5;
 
 Type population=array[1..(czas+1)] of real;
 
 Var pop0, pop1: population;
     t, i, j, k, koh: longint;
     as, ar, bs, br, ps,pr,N, age:real;
-    avgAge,varAge,avgN,varN: array[0..4] of real;
+    avgAge,varAge,avgN,varN: array[0..(seasons-1)] of real;
     winter: boolean;
     INFO: text;
 
@@ -29,7 +30,7 @@ begin
 assign(INFO,'wiek.txt');
 rewrite(INFO);
 append(INFO);
-writeln(INFO,'as ','ar ','bs ','br ','t ','avgN[t] ','varN[t] ','avgAge[t] ','varAge[t]');
+writeln(INFO,'as ','ar ','bs ','br ','t ','avgN(t) ','varN(t) ','avgAge(t) [months]','varAge(t)');
 close(INFO);
 for i:=0 to nas do
  begin
@@ -42,7 +43,7 @@ for i:=0 to nas do
   koh:=1;
   pop0[1]:=20;
   N:=20;
-  for t:=0 to 4 do
+  for t:=0 to (seasons-1) do
    begin
    avgN[t]:=0;
    varN[t]:=0;
@@ -51,8 +52,7 @@ for i:=0 to nas do
    end;
   for t:=1 to czas do
    begin
-   if ((t mod 5 = 4) or (t mod 5 = 0)) then winter:=true else winter:=false;
-   //winter:=false;
+   if ((t mod seasons = 4) or (t mod seasons = 0)) then winter:=true else winter:=false;
    age:=0;
    pr:=1/(1+exp(-(ar*N+br)));
    ps:=1/(1+exp(-(as*N+bs)));
@@ -69,22 +69,22 @@ for i:=0 to nas do
    if N<>0 then age:=age/N else age:=0;
    if t>100 then 
     begin
-    avgN[t mod 5]:=avgN[t mod 5]+N;
-    varN[t mod 5]:=varN[t mod 5]+N*N;
-    avgAge[t mod 5]:=avgAge[t mod 5]+age;
-    varAge[t mod 5]:=varAge[t mod 5]+age*age;
+    avgN[t mod seasons]:=avgN[t mod seasons]+N;
+    varN[t mod seasons]:=varN[t mod seasons]+N*N;
+    avgAge[t mod seasons]:=avgAge[t mod seasons]+age;
+    varAge[t mod seasons]:=varAge[t mod seasons]+age*age;
     end;
    gotoxy(1,whereY);
    write(i,' ',j,' ',t);
    end;
   append(INFO);
-  for t:=0 to 4 do
+  for t:=0 to (seasons-1) do
    begin
-   avgN[t]:=avgN[t]/((czas-100)/5);
-   varN[t]:=varN[t]/((czas-100)/5) - avgN[t]*avgN[t];
-   avgAge[t]:=avgAge[t]/((czas-100)/5);
-   varAge[t]:=varAge[t]/((czas-100)/5) - avgAge[t]*avgage[t];
-   writeln(INFO,as:7:5,' ',ar:7:5,' ',bs:7:5,' ',br:7:5,' ',t,' ',avgN[t]:7:5,' ',varN[t]:7:5,' ',avgAge[t]:7:5,' ',varAge[t]:7:5);
+   avgN[t]:=avgN[t]/((czas-100)/seasons);
+   varN[t]:=varN[t]/((czas-100)/seasons) - avgN[t]*avgN[t];
+   avgAge[t]:=avgAge[t]/((czas-100)/seasons);
+   varAge[t]:=varAge[t]/((czas-100)/seasons) - avgAge[t]*avgage[t];
+   writeln(INFO,as:7:5,' ',ar:7:5,' ',bs:7:5,' ',br:7:5,' ',t,' ',avgN[t]:7:5,' ',varN[t]:7:5,' ',avgAge[t]*12/5:7:5,' ',varAge[t]*(12/5)*(12/5):7:5);
    end;
   close(INFO);
   end;
